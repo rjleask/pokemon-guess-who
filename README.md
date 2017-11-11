1f55caba7ac84b6cff1baccf0984ed89091da246
Ross Blair <rblair33@gmail.com>
2017-11-11 10:53:31 -0500

    Initial folder structure and base working.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pokemon-guess-who
 MERN app that takes the essentials from guess who and adds an exciting pokemon twist
+
+1. Run yarn start in main folder and in client folder to grab necessary packages.
+2. create local instance of mongo database called pokemonguesswhodb (command line: mongo -> use pokemonguesswhodb)
+3. Run mongod on separate command line to ensure mongo is listening.
+4. Running yarn start in main folder should launch the app.
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("client/build"));
+// app.use(routes);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/pokemonguesswhodb",
+  {
+    useMongoClient: true
+  }
+);
+
+app.listen(PORT, function () {
+  console.log(`API Server now listening on PORT ${PORT}!`);
+});
