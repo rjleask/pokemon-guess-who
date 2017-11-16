109524b7b845b9ce1f3558a2473ed7814edb5073
Jamie <rjamie.leask@gmail.com>
2017-11-16 13:14:14 -0500

    added home page

--- client/src/components/loginBtn.js
+++ client/src/components/loginBtn.js
@@ -1,20 +1,12 @@
 import React from "react";
 import API from "../utils/API";
-// import {Link} from "react-router-dom";
 
 
 class Login extends React.Component{
-  handlerClick = () => {
-    // API.authLogin().then(console.log("LOGIN PAGE"));
-    // browserHistory.push("/login");
-    // window.location.href = "http://localhost:3000/api/auth/google";
-  };
   render(){
     return (
       // <button className="login-btn" onClick={this.handlerClick}>Login</button>
-
       <a href="http://localhost:3001/api/auth/google">Login</a>
-      // <Link to="/api/auth/login">Login</Link> 
     );
   
   }
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+import Login from "../../components/loginBtn";
+
+class Home extends Component {
+
+}
+export default Home;
