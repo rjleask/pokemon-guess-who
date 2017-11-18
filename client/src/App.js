425ca5dc2036a0d0ae163328b666054ba2eada88
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-11-18 10:31:59 -0500

    error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Pokedex from "./pages/Pokedex";
+import NotFound from "./pages/NotFound";
 
 const App = () =>
 	<Router>
@@ -9,6 +10,7 @@ const App = () =>
 	      <div>
 	        <Route exact path="/" component={Pokedex} />
 	        <Route exact path="/pokedex" component={Pokedex} />
+	        <Route component={NotFound} />
 	      </div>
       </div>
     </div>
---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Col, Row, Container } from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
+import "./NotFound.css";
+
+const NotFound = () =>
+  <Container fluid>
+    <Row>
+      <Col size="md-12">
+        <Jumbotron>
+          <h1 className="title">404 Page Not Found</h1>
+        <img src="https://vignette.wikia.nocookie.net/pokemon/images/3/3e/114Tangela.png/revision/latest/scale-to-width-down/200?cb=20140328205759" alt="Tangela" />
+        </Jumbotron>
+      </Col>
+    </Row>
+  </Container>;
+
+export default NotFound;
