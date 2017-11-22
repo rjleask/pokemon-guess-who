0d1828187342f8bcb6e781c2173fe52575f3d005
Ross Blair <rblair33@gmail.com>
2017-11-21 22:12:53 -0500

    Saves info to database on win.

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -17,7 +17,19 @@ module.exports = {
   getUniqueUser: function (req, res) {
     db.User
       .findOne({userCookie: req.params.cookie})
-      .then(dbModel => res.json(dbModel)
-        .catch(err => res.status(422).json(err)));
+      .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+  },
+  updateGameWin: function (req, res) {
+    console.log(req.body);
+    db.User
+      .findOneAndUpdate({ _id: req.body._id},
+        {highScore: req.body.highScore,
+        $push: {
+          pokemon: req.body.pokemon
+        }})
+      .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+
   }
 };
