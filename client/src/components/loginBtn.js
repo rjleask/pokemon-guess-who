ce9f73deeb03eb469698b377067dbec48b805368
Jamie <rjamie.leask@gmail.com>
2017-11-28 19:25:50 -0500

    test737373

--- client/src/components/loginBtn.js
+++ client/src/components/loginBtn.js
@@ -1,11 +1,17 @@
 import React from "react";
 import API from "../utils/API";
-
-
+let heroku = true;
+let url;
+if(heroku){
+  url = 'https://calm-hamlet-36261.herokuapp.com/api/auth/google';
+  
+}else{
+  url = 'http://localhost:3001/api/auth/google';
+}
 class Login extends React.Component{
   render(){
     return (
-      <a href="http://localhost:3001/api/auth/google"><button className="btn btn-primary">Login</button></a>
+      <a href={url}><button className="btn btn-primary">Login</button></a>
     );
   
   }
