3423e45ced1627a498a343b9cb245d3caf4c903b
Jamie <rjamie.leask@gmail.com>
2017-11-28 19:49:40 -0500

    test007

--- client/src/components/logoutBtn.js
+++ client/src/components/logoutBtn.js
@@ -1,11 +1,17 @@
 import React from "react";
 import API from "../utils/API";
-
-
+let heroku = true;
+let url;
+if(heroku){
+  url = 'https://calm-hamlet-36261.herokuapp.com/api/auth/logout';
+  
+}else{
+  url = 'http://localhost:3001/api/auth/logout';
+}
 class Logout extends React.Component{
   render(){
     return (
-      <p><a href="http://localhost:3001/api/auth/logout"><button className="btn btn-primary">Logout</button></a></p>
+      <p><a href={url}><button className="btn btn-primary">Logout</button></a></p>
     );
   
   }
