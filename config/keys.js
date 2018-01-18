833cc2ed559c05493dc16294eeba9e754f546070
Jamie <rjamie.leask@gmail.com>
2018-01-18 12:29:33 -0500

    changed keys file

--- config/keys.js
+++ config/keys.js
@@ -1,18 +1,3 @@
 // add this file to .gitignore
 
-module.exports = {
-  google: {
-    clientID:
-      "124016383603-tbo3h4qnrhr1tjo10abbrsbqjdbregdm.apps.googleusercontent.com",
-    clientSecret: "W-s72KG_zDQSHjcgqhecRSAh"
-  },
-  // database connection
-  mongodb: {
-    //   myDB: 'mongodb://localhost/pokemon'
-    myDB: "mongodb://localhost/pokemonguesswhodb"
-  },
-  // encript cookie key
-  session: {
-    cookieKey: "encryptedcookie"
-  }
-};
+module.exports = {};
